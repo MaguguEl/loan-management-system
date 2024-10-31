79015ccd130faa3403b0793d7ddb5fca7d36a833
gugu-zen <bsc-inf-16-17@unima.ac.mw>
2024-10-31 19:56:00 +0200

    added bool function

--- lib/navigation/main_screen.dart
+++ lib/navigation/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:loan_management_system/features/dashboard/dashboard_screen.dart';
-import 'package:loan_management_system/features/member_management/member_screen.dart';
 import 'package:loan_management_system/features/reports/reports_screen.dart';
 import 'package:loan_management_system/navigation/admin_profile_screen.dart';
 import 'package:loan_management_system/navigation/posts_screen.dart';
@@ -15,6 +14,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int currentTab = 0;
+  bool isAdminActive = false; 
 
   final List<Widget> screens = [
     HomeScreen(),
@@ -63,19 +63,30 @@ class _MainScreenState extends State<MainScreen> {
             _buildIconButton(Icons.home_filled, 'Home', 0),
             _buildIconButton(Icons.dashboard, 'Dashboard', 1),
             _buildIconButton(Icons.table_view, 'Reports', 2),
-            _buildIconButton(Icons.person, 'Admin', 3),
+            _buildIconButton(
+              isAdminActive ? Icons.person : Icons.person_outline_rounded, 
+              'Admin',
+              3,
+              isAdmin: true, 
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildIconButton(IconData icon, String label, int tabIndex) {
+  Widget _buildIconButton(IconData icon, String label, int tabIndex, {bool isAdmin = false}) {
     return GestureDetector(
       onTap: () {
         setState(() {
           currentTab = tabIndex;
           currentScreen = screens[tabIndex];
+
+          if (isAdmin) {
+            isAdminActive = !isAdminActive; 
+          } else {
+            isAdminActive = false;
+          }
         });
       },
       child: Column(
@@ -89,7 +100,7 @@ class _MainScreenState extends State<MainScreen> {
             label,
             style: TextStyle(
               color: currentTab == tabIndex ? const Color(0xFF305CDE) : Colors.grey,
-              fontSize: 13
+              fontSize: 13,
             ),
           ),
         ],
